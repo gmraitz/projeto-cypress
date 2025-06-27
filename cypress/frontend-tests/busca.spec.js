235ba590637c19fab6d9bf4aab5a0223f46d1154
gmraitz <guilhermeraitz@hotmail.com>
2025-06-26 23:28:44 -0300

    mensagem do commit aqui

--- cypress/frontend-tests/busca.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Search Functionality', () => {
-  it('Deve realizar busca e mostrar resultados', () => {
-    cy.visit('/');
-    cy.get('input#search').type('shirt{enter}');
-    cy.url().should('include', 'catalogsearch/result');
-    cy.get('.products.wrapper').should('exist');
-    cy.get('.product-item').its('length').should('be.gte', 1);
-  });
-});
--- cypress/frontend-tests/cadastro.spec.js
+++ cypress/frontend-tests/cadastro.spec.js
@@ -1,14 +1,27 @@
-describe('Create Account Page', () => {
-  beforeEach(() => {
-    cy.visit('/customer/account/create/');
-  });
+describe('Cadastro de novo usuário - Magento', () => {
+  it('Deve preencher e submeter o formulário com sucesso', () => {
+    // Visita a página de criação de conta
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/');
 
-  it('Deve carregar página de criação de conta', () => {
-    cy.get('form#form-validate').should('exist');
-  });
+    // Gera dados dinâmicos para evitar conflito de e-mails já usados
+    const timestamp = Date.now();
+    const firstName = 'João';
+    const lastName = 'Teste';
+    const email = `usuario${timestamp}@teste.com`;
+    const password = 'Senha@123456';
 
-  it('Deve validar campos obrigatórios', () => {
-    cy.get('button#form-validate').click();
-    cy.get('.mage-error').should('exist');
+    // Preenche o formulário de cadastro
+    cy.get('#firstname').type(firstName);
+    cy.get('#lastname').type(lastName);
+    cy.get('#email_address').type(email);
+    cy.get('#password').type(password);
+    cy.get('#password-confirmation').type(password);
+
+    // Clica no botão para criar conta
+    cy.get('button[title="Create an Account"]').click();
+
+    // Valida que o usuário foi redirecionado para a conta
+    cy.url().should('include', '/customer/account');
+    cy.contains(`Hello, ${firstName} ${lastName}!`).should('be.visible');
   });
 });
